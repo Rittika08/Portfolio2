dd665c688ae63a694461299fc1ff1036984732d1
Rittika08 <sahurittika8@gmail.com>
2024-12-17 13:29:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// toggle icon navbar
+// scroll sections
+//let sections= document.querySelectorAll('section');
+//let navLinks= document.querySelectorAll('header nav a');
+
+//window.onscroll = () => {
+    //sections.forEach(sec => {
+        //let top= window.scrollY;
+        //let Offset = sec.offsetTop - 100;
+        //let height= sec.offsetHeight;
+        //let id= sec.getAttribute('id');
+
+        //if(top >= offset && top< offset + height){
+            //navLinks.forEach()
+    
+    
+// sticky header
+
+//let header = document.querySelector('header');
+    //header.classList.toggle('sticky' , window.scrollY > 100);//
+// remove toggle icon and navbar when click navbar links (scroll)
+// animation footer on scroll
